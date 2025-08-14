7b8eb162825738e3dbb086cbe98cb179d1d56675
mizegret <mizegret@example.com>
2025-08-14 10:23:11 +0900

    feat: add header and footer components...

---
+++ mps-web/src/layouts/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer
+    uno-border="t gray-200"
+    class="w-full bg-white"
+  >
+    <div
+      uno-text="sm gray-600 center"
+      class="container mx-auto app-pad"
+    >
+      <p>&copy; 2025 MPS Product Survey. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
